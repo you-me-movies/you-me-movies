adfbc6b24b57183f1c58fc1d41d10b3501e822df
byeongjulee222 <dave.juya777@gmail.com>
2019-11-27 14:13:31 +0900

    eb config setting

--- youme/settings/base.py
+++ youme/settings/base.py
@@ -128,10 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static' # 추가
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'crud', 'assets'),
 ]
+
 
 AUTH_USER_MODEL = 'accounts.User'
 
